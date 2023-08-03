539ab03b4577832ab071201901098b3a58646962
Abhishek Gole <123919399+gole-262001@users.noreply.github.com>
2023-08-03 15:38:06 +0530

    Create 4. Sort an array of 0s, 1s and 2s.cpp

---
+++ C++/4. Sort an array of 0s, 1s and 2s.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int> arr(3 , 0);
+        for(int i = 0; i < nums.size(); i++)
+        {
+                if(nums[i] == 0) arr[0]++;
+                else if(nums[i] == 1) arr[1]++;
+                else arr[2]++;
+        }
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(arr[0] != 0)
+            {
+                nums[i] = 0;
+                arr[0]--;
+            } 
+            else if(arr[1] != 0) {
+                nums[i] = 1;
+                arr[1]--;
+            }
+            else {
+                nums[i] = 2;
+                arr[2]--;
+            } 
+        }
+    }
+};
