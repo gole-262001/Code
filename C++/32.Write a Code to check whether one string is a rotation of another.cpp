119ca5500f7d393141b4b9e387049cf58e52bf0f
Abhishek Gole <123919399+gole-262001@users.noreply.github.com>
2023-08-21 17:04:23 +0530

    Update and rename 31.Write a Code to check whether one string is a rotation of another.cpp to 32.Write a Code to check whether one string is a rotation of another.cpp

--- C++/32.Write a Code to check whether one string is a rotation of another.cpp
+++ C++/32.Write a Code to check whether one string is a rotation of another.cpp
@@ -40,3 +40,31 @@ public:
         
     }
 };
+// 2nd solution
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()) return false;
+        int n = s.size();
+        queue<char> q1;
+        queue<char> q2;
+        for(int i = 0; i < n; i++){
+            q1.push(s[i]);
+            q2.push(goal[i]);
+        }
+        int k = goal.size();
+        while( k > 0){
+            char temp = q2.front();
+            q2.pop();
+            q2.push(temp);
+            if(q1 == q2){
+                return true;
+            }
+            k--;
+        }
+        return false;
+
+
+    }
+};
+// 3rd solution
